0b1a5b09cbe77de2c90271b9485ddad643ad12e1
mennarihan5 <menna1986@live.com>
2024-09-09 12:34:00 +0300

    changed title design

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss';
 import ConnectBtn from '../ConnectBtn/connectBtn';
 import heroImg from '../../assets/content-structure-concept-illustration.png';
+import img from '../../assets/m010t0520_f_shape_element_31aug22.png';
 import { Link } from 'react-scroll';
 
 const Hero = () => {
@@ -8,10 +9,12 @@ const Hero = () => {
     <section className={styles['hero-container']}>
         <div className={styles['left-side']}>
           <div className={styles['text-wrapper']}>
-            <h2 className={styles.title}>
-              <span className={styles.name}> Menna Rihan,</span> <br />
-             frontend developer.
-            </h2>
+            <div className={styles.title}>
+              <span className={styles.name}> MENNA RIHAN,</span> 
+              <br />
+              <br />
+             <p>frontend developer.</p>
+            </div>
             <p className={styles.subtitle}>
               I’m a Front-End Web Developer with expertise in crafting responsive, pixel-perfect interfaces that prioritize a seamless and intuitive user experience. 
               I’m dedicated to building web solutions that are both visually appealing and highly functional.
@@ -30,11 +33,6 @@ const Hero = () => {
               </ConnectBtn>
           </div>
         </div>
-        {/* <div className={styles['right-side']}>
-          <div className={styles['img-wrapper']}>
-            <img src={heroImg} alt="Hero" />
-          </div>
-        </div> */}
     </section>
   )
 }
